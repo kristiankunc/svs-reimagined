32523976139b9fd9e9de1b4d0dc94eb4d5b6c01c
Kristián Kunc <kristian.kunc@gmail.com>
2025-04-29 16:51:09 +0200

    dump secret key to file

--- svs/env_loader.py
+++ svs/env_loader.py
@@ -6,7 +6,7 @@ from .base_dir import BASE_DIR
 ENV_VARS = {
     "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY": None,
     "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET": None,
-    "SECRET_KEY": secrets.token_hex(32),
+    "SECRET_KEY": None,
     "DEBUG": False,
     "ALLOWED_HOSTS": ["localhost", "127.0.0.1"],
 }
@@ -16,6 +16,18 @@ def load_env() -> dict:
     variables = {}
     if os.path.exists(os.path.join(BASE_DIR, ".env")):
         load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+    secret_key_path = os.path.join(BASE_DIR, "data", "secret_key.txt")
+
+    if os.path.exists(secret_key_path):
+        with open(secret_key_path, "r") as f:
+            secret_key = f.read().strip()
+            os.environ["SECRET_KEY"] = secret_key
+    else:
+        with open(secret_key_path, "w") as f:
+            secret_key = secrets.token_urlsafe(32)
+            f.write(secret_key)
+            os.environ["SECRET_KEY"] = secret_key
 
     for key, default_value in ENV_VARS.items():
         if key in os.environ:
